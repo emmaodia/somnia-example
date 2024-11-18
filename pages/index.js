629976e48a850dc75776b8670cd86e8f7a7cc805
Emmanuel Oaikhenan <odia.emma@gmail.com>
2024-11-18 23:10:46 +0100

    Contract interaction.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
 import { useEffect, useState } from "react";
 
-import { createWalletClient, custom, defineChain } from "viem";
+import {
+  createPublicClient,
+  http,
+  parseEther,
+  createWalletClient,
+  custom,
+  formatEther,
+  parseAbiItem,
+  defineChain,
+} from "viem";
+import { ABI } from "../abi";
+const CONTRACT_ADDRESS = "0x2e7f682863a9dcb32dd298ccf8724603728d0edd";
 
 const SOMNIA = defineChain({
   id: 50311,
@@ -20,10 +31,16 @@ const SOMNIA = defineChain({
   },
 });
 
+const publicClient = createPublicClient({
+  chain: SOMNIA,
+  transport: http(),
+});
+
 export default function Home() {
   const [client, setClient] = useState(null);
   const [address, setAddress] = useState(null);
   const [connected, setConnected] = useState(false);
+  const [houseBalance, setHouseBalance] = useState("");
 
   const connectToMetaMask = async () => {
     if (typeof window !== "undefined" && window.ethereum !== undefined) {
@@ -48,6 +65,39 @@ export default function Home() {
     }
   };
 
+  const fetchSymbol = async () => {
+    try {
+      const balance = await publicClient.readContract({
+        address: CONTRACT_ADDRESS, // New contract address
+        abi: ABI,
+        functionName: "symbol",
+      });
+      setHouseBalance(balance);
+    } catch (error) {
+      console.error("Failed to fetch house balance:", error);
+    }
+  };
+
+  const sendTxn = async () => {
+    try {
+      const hash = await client.sendTransaction({
+        account: address,
+        to: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
+        value: 1000000000000000000n,
+      });
+
+      console.log(hash);
+    } catch (error) {
+      console.error(error, "failed");
+    }
+  };
+
+  useEffect(() => {
+    if (client && address) {
+      fetchSymbol();
+    }
+  }, [client, address]);
+
   return (
     <div className="flex flex-col p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {!connected ? (
@@ -58,7 +108,67 @@ export default function Home() {
           Connect Wallet
         </button>
       ) : (
-        <div>Hello, {address}</div>
+        <div>
+          <p>Connected as: {address}</p>
+          <p>House Balance: {houseBalance} Ether</p>
+          <button className="mt-5 px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Faucet
+          </button>
+          <button
+            onClick={sendTxn}
+            className="px-5 py-2.5 mx-5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Test TXN
+          </button>
+          <div class="mt-2">
+            <form
+              // onSubmit={handleSwap}
+              className="flex max-w-md flex-col gap-4"
+            >
+              <div className="mt-3">
+                <label
+                  htmlFor="amount-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Receiver
+                </label>
+                <input
+                  type="text"
+                  id="amount-input"
+                  // value={amount}
+                  // onChange={(e) => setAmount(e.target.value)}
+                  placeholder="Enter Recipient Address"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="amount-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Amount
+                </label>
+                <input
+                  type="text"
+                  id="amount-input"
+                  // value={amount}
+                  // onChange={(e) => setAmount(e.target.value)}
+                  placeholder="Enter amount in ETH"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                // disabled={loading}
+              >
+                {/* {loading ? "Swapping..." : "Swap"} */}Transfer
+              </button>
+            </form>
+          </div>
+        </div>
       )}
     </div>
   );
