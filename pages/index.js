b68ce5d2a90ac6426e0e00f289e66b878e4f3a68
Emmanuel Oaikhenan <odia.emma@gmail.com>
2024-11-19 10:53:45 +0100

    ERC20 Txns.

--- pages/index.js
+++ pages/index.js
@@ -104,26 +104,37 @@ export default function Home() {
     }
   };
 
-  const nativeERC = async () => {
-    const { request } = await publicClient.simulateContract({
-      address: CONTRACT_ADDRESS,
-      abi: ABI,
-      functionName: "transfer",
-      // value: 1000000000000000000n,
-      args: ["0xa42872B5359F6e3905BB031df62C3AADde532933", 100000000000000000n],
-      // parseEther(betAmount),
-      account: address,
-    });
-
-    console.log(request);
-    const hash = await client.writeContract(request);
-    console.log(hash);
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Input value:", toValue, amountValue);
     sendTxn();
+  };
+
+  const nativeERC = async () => {
+    console.log(amountValue, toValue);
+    try {
+      const { request } = await publicClient.simulateContract({
+        address: CONTRACT_ADDRESS,
+        abi: ABI,
+        functionName: "transfer",
+        // value: 1000000000000000000n,
+        args: [toValue, amountValue],
+        // parseEther(betAmount),
+        account: address,
+      });
+
+      console.log(request);
+      const hash = await client.writeContract(request);
+      console.log(hash);
+    } catch (error) {
+      console.error(error, "failed");
+    }
+  };
+
+  const handleSubmitERC = (e) => {
+    e.preventDefault();
+    console.log("Input value:", toValue, amountValue);
+    nativeERC();
   };
 
   useEffect(() => {
@@ -160,11 +171,62 @@ export default function Home() {
           >
             Test ERC20
           </button>
-          <div class="mt-2">
+          <div class="mt-10">
             <form
               onSubmit={handleSubmit}
               className="flex max-w-md flex-col gap-4"
             >
+              Native Token Txn
+              <div className="mt-3">
+                <label
+                  htmlFor="amount-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Receiver
+                </label>
+                <input
+                  type="text"
+                  id="amount-input"
+                  value={toValue}
+                  onChange={handleTo}
+                  placeholder="Enter Recipient Address"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="amount-input"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Amount
+                </label>
+                <input
+                  type="text"
+                  id="amount-input"
+                  value={amountValue}
+                  onChange={handleAmount}
+                  placeholder="Enter amount in ETH"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                // disabled={loading}
+              >
+                {/* {loading ? "Swapping..." : "Swap"} */}Transfer
+              </button>
+            </form>
+          </div>
+
+          <div class="mt-10">
+            <form
+              onSubmit={handleSubmitERC}
+              className="flex max-w-md flex-col gap-4"
+            >
+              ERC20 Token Txn
               <div className="mt-3">
                 <label
                   htmlFor="amount-input"
